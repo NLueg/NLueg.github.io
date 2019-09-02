ea8361474d3122bde826c28c9ceb1d7d5fc4299c
nlueg <nicolueg@yahoo.de>
2019-09-02 09:43:12 +0200

    fixed url-problem. but they are still no links

--- _plugins/jekyll_scholar.rb
+++ _plugins/jekyll_scholar.rb
@@ -1,10 +1,12 @@
-# require 'jekyll/scholar'
-# require 'uri'
+require 'jekyll/scholar'
+require 'uri'
 
-# module MarkdownFilter
-#   class Markdown < BibTeX::Filter
-#     def apply(value)
-# value.to_s.gsub(URI.regexp(['http','https','ftp'])) { |c| "[#{$&}](#{$&})" }
-#     end
-#   end
-# end
+module MarkdownFilter
+  class Markdown < BibTeX::Filter
+    def apply(value)
+        value.to_s.gsub(URI.regexp(['http','https','ftp'])) { |c| "<a href=\"#{$&}\">#{$&}</a>" }
+    end
+  end
+end
+
+Liquid::Template.register_filter(MarkdownFilter)
